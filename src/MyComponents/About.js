76231f28f342d98775dc963f78a24139b5d324d9
AmanRoy <acc.amanroy@gmail.com>
2022-03-21 09:46:48 +0530

    initial comment

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>This is an about MyComponents
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio explicabo nemo ullam repellendus sapiente minima, a ea atque quos harum delectus assumenda magnam facilis debitis dolore ipsam non et laborum ad beatae, consequatur velit.</p>
+    </div>
+  )
+}
